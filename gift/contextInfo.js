daf090d154c65ecb095c57d721992a0ea690f5ab
Lucvoice <simkondabrand@gmail.com>
2026-09-10 12:51:49 +0300

    Update contextInfo.js

--- gift/contextInfo.js
+++ gift/contextInfo.js
@@ -1,7 +1,7 @@
 const { getSetting } = require("./database/settings");
 
 async function getContextInfo(mentionedJid = []) {
-    const botName = await getSetting("BOT_NAME") || "𝐀𝐓𝐀𝐒𝐒𝐀-𝐌𝐃";
+    const botName = await getSetting("BOT_NAME") || "𝐋𝐔𝐊𝐀-𝐗𝐌𝐃";
     const channelJid = await getSetting("NEWSLETTER_JID") || "120363426409647211@newsletter";
     return {
         mentionedJid,
