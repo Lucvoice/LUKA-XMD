a3ad7a1b7d707bc192202764d16f58746f9fa938
Lucvoice <simkondabrand@gmail.com>
2026-09-10 18:48:04 +0300

    Update sports.js

--- lukah/sports.js
+++ lukah/sports.js
@@ -1,10 +1,10 @@
-const { gmd } = require("../gift");
+const { gmd } = require("../luka");
 const axios = require("axios");
 const {
   generateWAMessageContent,
   generateWAMessageFromContent,
 } = require("gifted-baileys");
-const { getSetting } = require("../gift/database/settings");
+const { getSetting } = require("../luka/database/settings");
 
 const SPORTS_API_BASE = "https://apiskeith.top";
 
