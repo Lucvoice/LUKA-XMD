5dda28e2e74fa033b49c3e5c1951fab747cea556
Lucvoice <simkondabrand@gmail.com>
2026-09-10 12:41:34 +0300

    Update serializer.js

--- gift/connection/serializer.js
+++ gift/connection/serializer.js
@@ -148,7 +148,7 @@ const serializeMessage = async (ms, Gifted, settings = {}) => {
         command,
         args,
         q: args.join(' '),
-        pushName: ms.pushName || (ms.key.fromMe ? Gifted.user?.name : null) || '𝐋𝐔𝐊𝐀-𝐌𝐃 User',
+        pushName: ms.pushName || (ms.key.fromMe ? Gifted.user?.name : null) || '𝐋𝐔𝐊𝐀-𝐗𝐌𝐃 User',
         quoted,
         repliedMessage,
         mentionedJid,
