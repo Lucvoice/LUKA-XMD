f5c562b953c59893937139e116b97f88bc840f74
Lucvoice <simkondabrand@gmail.com>
2026-09-10 18:52:24 +0300

    Update updater.js

--- lukah/updater.js
+++ lukah/updater.js
@@ -1,4 +1,4 @@
-const { gmd, copyFolderSync } = require("../gift");
+const { gmd, copyFolderSync } = require("../luka");
 const axios = require("axios");
 const fs = require("fs");
 const path = require("path");
