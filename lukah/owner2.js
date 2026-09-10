16b0c1fa517f634e1fc3c28cd007ab571f0b2c18
Lucvoice <simkondabrand@gmail.com>
2026-09-10 18:39:39 +0300

    Refactor imports to use 'luka' instead of 'gift'

--- lukah/owner2.js
+++ lukah/owner2.js
@@ -1,4 +1,4 @@
-const { gmd, commands, getSetting } = require("../gift");
+const { gmd, commands, getSetting } = require("../luka");
 const fs = require("fs").promises;
 const fsA = require("node:fs");
 const { S_WHATSAPP_NET } = require("gifted-baileys");
@@ -9,7 +9,7 @@ const {
   groupCache,
   getGroupMetadata,
   cachedGroupMetadata,
-} = require("../gift/connection/groupCache");
+} = require("../luka/connection/groupCache");
 
 const { exec: _shellExec } = require("child_process");
 
@@ -61,8 +61,8 @@ gmd(
 
     await react("⏳");
     try {
-      const gift = require("../gift");
-      const _rawDb = require("../gift/database/database").DATABASE;
+      const gift = require("../luka");
+      const _rawDb = require("../luka/database/database").DATABASE;
       const settings = await gift.getAllSettings();
       const { getSetting, setSetting, getAllSettings, commands } = gift;
       const prefix = settings.PREFIX;
